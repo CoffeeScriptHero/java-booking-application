44203d59728e6e4a7aa388eec60b1daacbb0a74f
Dariy Pavlyk <dariypavlyk@gmail.com>
2022-02-11 23:25:16 +0200

    flight class and collection created

---
+++ src/flights/FlightCollection.java
@@ -0,0 +1,43 @@
+package flights;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightCollection implements FlightDAO {
+
+  private static final FlightCollection flightCollection = new FlightCollection();
+  private final List<Flight> flights = new ArrayList<>();
+
+  private FlightCollection() {
+  }
+
+  public static FlightCollection instanceOf() {
+    return flightCollection;
+  }
+
+
+  @Override
+  public List<Flight> getAllFlights() {
+    return flights;
+  }
+
+  @Override
+  public Flight getFlightById(int id) {
+    return flights.get(id);
+  }
+
+  @Override
+  public void deleteFlight(int id) {
+    flights.removeIf(flight -> flight.getId() == id);
+  }
+
+  @Override
+  public void saveFlight(Flight flight) {
+    if (flights.size() >= 1 && flights.stream().anyMatch(flight::equals)) {
+      int replaceIndex = flights.indexOf(flight);
+      flights.set(replaceIndex, flight);
+    } else {
+      flights.add(flight);
+    }
+  }
+}
